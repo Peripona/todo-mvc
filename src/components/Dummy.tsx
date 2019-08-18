123b97ffbd1b696925b977e7e983edcba7f58589
Tarandeep Singh <peripona@yahoo.com>
2019-08-18 20:07:51 +0200

    Use makeStyles for css in js, also possible to use scss with makeStyles

--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -3,12 +3,27 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
+  
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://material-ui.com/">    
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
@@ -20,14 +35,20 @@ function Copyright() {
   );
 }
 
+const DummyButton = () => {
+    const classes = useStyles();
+    return <Button className={classes.root}>Hook</Button>;
+}
+
 export default function Dummy() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Box my={4}>
-        <Typography variant="h4" component="h1" color="textPrimary" gutterBottom>
+        <Typography variant="h2" component="h1" color="textPrimary" gutterBottom>
           Create React App v4-beta example
         </Typography>
         <Copyright />
+        <DummyButton />
       </Box>
     </Container>
   );
